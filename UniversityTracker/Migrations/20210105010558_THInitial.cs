65d4364cd1739a207b8bc5032aac36f1d2009155
Tawnee Harris <tawneeh@icloud.com>
2021-01-04 17:11:51 -0800

    add University Database creation

---
+++ UniversityTracker/Migrations/20210105010558_THInitial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniversityTracker.Migrations
+{
+    public partial class THInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EnrollmentDate",
+                table: "Students",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EnrollmentDate",
+                table: "Students",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
