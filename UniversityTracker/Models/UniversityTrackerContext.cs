7b3634c2393b95c715c6adcc03fa4799cf48a0cf
Tawnee Harris <tawneeh@icloud.com>
2021-01-05 15:46:06 -0800

    update Database Sets
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>
    Co-authored-by: David Sterry <sterry.david@gmail.com>

--- UniversityTracker/Models/UniversityTrackerContext.cs
+++ UniversityTracker/Models/UniversityTrackerContext.cs
@@ -7,6 +7,9 @@ namespace UniversityTracker.Models
     public virtual DbSet<Student> Students {get; set;}
     public DbSet<Course> Courses { get; set;  }
     public DbSet<Enrollment> Enrollment { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Major> Majors { get; set; }
+    public DbSet<Curriculum> Curriculums { get; set; }
 
     public UniversityTrackerContext(DbContextOptions options) : base(options) { }
   }
